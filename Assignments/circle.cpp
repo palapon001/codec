a963e5bcc7964035a10c165fe4cd86aa001c20fd
palapon001 <34773021+palapon001@users.noreply.github.com>
2020-07-23 12:12:44 +0700

    Add files via upload

---
+++ Assignments/circle.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+ 
+int main()
+{
+     
+    float r;
+     
+    printf(" Input value of Radius : ");
+    scanf("%f", &r);
+     
+    printf("\n The area of a circle is %.2f \n\n", M_PI * (r * r) );
+     
+    return 0;
+}
---
+++ Assignments/square.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float side;
+ 
+    printf("\nInput value of side : ");
+    scanf("%f", &side);
+     
+    printf("Area of a square is %.2f \n\n", side * side);
+ 
+    return 0;
+}
